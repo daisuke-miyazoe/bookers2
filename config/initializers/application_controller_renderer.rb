f36c6ae49796a58eb1bb87be3f49d19c9ea42f2d
daisuke miyazoe <hndfavodaismi@gmail.com>
2020-04-30 20:38:22 +0900

    修正完了

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '30425d2ed791c238ba44c0169f8ebcecc542c7feb4d0e8639470dfb75a2e9b55f6c44bef0268eb6a9c20a41c528b2ddc036e7cdea619c8e67dc055c2436db017'
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  root 'homes#index'
+  get '/home/about', to: 'homes#about'
+
+  resources :books
+  resources :users
+  
+end
